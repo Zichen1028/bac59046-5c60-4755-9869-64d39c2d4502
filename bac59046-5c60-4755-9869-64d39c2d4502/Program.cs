496c20acf6f01060318da10daf7b9be6c30c32d9
Zichen1028 <Simonwu0411@gmail.com>
2024-02-15 00:50:52 +1100

    Add lines to read input file
    Read and output the content from the input txt file

--- bac59046-5c60-4755-9869-64d39c2d4502/Program.cs
+++ bac59046-5c60-4755-9869-64d39c2d4502/Program.cs
@@ -1,3 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using static System.Net.Mime.MediaTypeNames;
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"../../../Input.txt";
+        string input = File.ReadAllText(filePath);
+
+        // Print the input value
+        Console.WriteLine("Output is: " + input);
+    }
+}
