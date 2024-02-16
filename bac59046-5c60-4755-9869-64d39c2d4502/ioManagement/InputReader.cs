04e910ca964a435ead5217b0b691e76da326bcc5
Zichen1028 <64767085+Zichen1028@users.noreply.github.com>
2024-02-16 19:32:19 +1100

    version 2 - init commit (#2)
    Reconstruct the solution with dedicated class for single responsibility

---
+++ bac59046-5c60-4755-9869-64d39c2d4502/ioManagement/InputReader.cs
@@ -0,0 +1,11 @@
+﻿public class InputReader
+{
+	public int[] readInput()
+	{
+        string filePath = @"../../../Input.txt";
+        string inputText = File.ReadAllText(filePath);
+
+        string[] input = inputText.Split(' ');
+        return Array.ConvertAll(input, int.Parse);
+    }
+}
