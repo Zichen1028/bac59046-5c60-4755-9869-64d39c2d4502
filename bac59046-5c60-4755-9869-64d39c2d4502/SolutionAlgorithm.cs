d1e81cf56840b2e6aec11c047b2d6e53eb965e99
Zichen1028 <Simonwu0411@gmail.com>
2024-02-16 19:25:08 +1100

    version 2 - init commit
    Reconstruct the solution with dedicated class for single responsibility

---
+++ bac59046-5c60-4755-9869-64d39c2d4502/SolutionAlgorithm.cs
@@ -0,0 +1,45 @@
+﻿public class SolutionAlgorithm
+{
+    /**
+        * Algorithm of the solution
+        * @param inputArr      the input array to be calculated
+        * @return outputArr    the output array that contains two nums as the result
+        */
+    public int[] calculateSolution(int[] inputArr)
+    {
+        // start calculating here
+        int subSeqLength = 1;
+        int longest = 1;
+        int indexStart = 0;
+        int indexEnd = 0;
+
+        for (int i = 0; i < inputArr.Length - 1; i++)
+        {
+            if (inputArr[i] < inputArr[i + 1])
+            {
+                ////if this is the case, increment
+                subSeqLength++; 
+
+                //we assign the longest and new bounds
+                if (subSeqLength > longest) 
+                {
+                    longest = subSeqLength;
+
+                    //this is to make sure the index start is correct
+                    indexStart = i + 2 - subSeqLength;
+                    indexEnd = i + 2;
+                }
+            }
+            else
+            {
+                //otherwise initiate the straight length as 1
+                subSeqLength = 1;
+            } 
+        }
+
+        int[] outputArr = { indexStart, indexEnd };
+        return outputArr;
+    }
+}
+
+
---
+++ bac59046-5c60-4755-9869-64d39c2d4502/ioManagement/OutputWriter.cs
@@ -0,0 +1,22 @@
+﻿public class OutputWriter
+{
+	/**
+	 * process the output as the results
+	 * @param outputArr		an array that contains two ints as the results
+	 *						which indicates the starting index and the end index
+	 *						of the subsequence
+	 * @param originArr		an array that contains the original ints from the 
+	 *						input content
+	 */
+	public void processOutputResult(int[] outputArr, int[] originArr)
+	{
+		int indexStart = outputArr[0];
+		int indexEnd = outputArr[1];
+        for (int i = indexStart; i < indexEnd; i++)
+        {
+            Console.WriteLine(originArr[i]);
+        }
+    }
+}
+
+
