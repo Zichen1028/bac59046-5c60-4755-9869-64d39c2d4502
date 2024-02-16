e43fa27f50c24e9d1fa6e2c94fd95fa2a95dd560
Zichen1028 <64767085+Zichen1028@users.noreply.github.com>
2024-02-16 12:15:26 +1100

    Feature version 1 (#1)
    * Add lines to read input file
    
    Read and output the content from the input txt file
    
    * Create base version of the solution
    
    Create the very first version of the solution, need enhancement.
    
    * Create dotnet.yml

--- bac59046-5c60-4755-9869-64d39c2d4502/Program.cs
+++ bac59046-5c60-4755-9869-64d39c2d4502/Program.cs
@@ -1,3 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        string filePath = @"../../../Input.txt";
+        string inputText = File.ReadAllText(filePath);
 
+        string[] inputArr = inputText.Split(' ');
+        int[] arr = Array.ConvertAll(inputArr, int.Parse);
+
+        // start calculating here
+        int subSeqLength = 1;
+        int longest = 1;
+        int indexStart = 0;
+        int indexEnd = 0;
+
+        for (int i = 0; i < arr.Length - 1; i++)
+        {
+            if (arr[i] < arr[i + 1])//We need to check if the current is equal to the next
+            {
+                subSeqLength++;//if it is we increment
+                if (subSeqLength > longest)//we assign the longest and new bounds
+                {
+                    longest = subSeqLength;
+                    indexStart = i + 2 - subSeqLength;//make sure the index start is correct
+                    indexEnd = i + 2;
+                }
+            }
+            else
+            {
+                subSeqLength = 1;//else re-initiate the straight length
+            }
+        }
+
+        for (int i = indexStart; i < indexEnd; i++)
+        {
+            Console.WriteLine(arr[i]);
+        }
+        
+    }
+}
