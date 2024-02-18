6b38079ff5fa1d76825ff50c0192905da01f6526
Zichen1028 <64767085+Zichen1028@users.noreply.github.com>
2024-02-18 17:45:00 +1100

    Feature version 3 add unit tests (#13)
    * feature-version-3: move algorithm into a separate directory
    
    * feature-version-3-create-tests-cover-exception
    
    added many unit tests to cover different cases including exceptional cases in the input file
    
    * feature-version-3-create-the complete-unit-tests
    
    * remove print line in a test

--- UnitTests/SolutionAlgorithmTests.cs
+++ UnitTests/SolutionAlgorithmTests.cs
@@ -4,6 +4,10 @@ public class SolutionAlgorithmTests
 {
     private SolutionAlgorithm _solutionAlgorithm { get; set; } = null!;
 
+    private string completeTestCaseInputFilePath = @"../../../CompleteTestCaseInputFile.txt";
+
+    private string completeTestCaseOutputFilePath = @"../../../CompleteTestCaseOutputFile.txt";
+
     [SetUp]
     public void Setup()
     {
@@ -11,30 +15,49 @@ public class SolutionAlgorithmTests
     }
 
     [Test]
-    public void CalculateSolution_ShouldReturnExpectedOutputTest1()
+    public void CompleteTestCase_ShouldAllReturnExpectedArr()
     {
-        // Assign
-        int[] inputTest = { 6, 2, 4, 6, 1, 5, 9, 2 };
-        int[] expectedResultArr = { 1, 4 };
+        List<string> inputLinesList = readLinesFromFile(completeTestCaseInputFilePath);
+        List<string> outputLinesList = readLinesFromFile(completeTestCaseOutputFilePath);
 
-        // Act
-        int[] resultArr = _solutionAlgorithm.calculateSolution(inputTest);
+        for (int i = 0; i < inputLinesList.Count; i++) 
+        {
+            int[] inputIntsArr = readInputIntsArr(inputLinesList[i]);
+            int[] expectedOutputResultArr = readInputIntsArr(outputLinesList[i]);
+            int[] resultIndexArr = _solutionAlgorithm.calculateSolution(inputIntsArr);
 
-        // Assert
-        Assert.That(resultArr, Is.EqualTo(expectedResultArr));
+            int indexStart = resultIndexArr[0];
+            int indexEnd = resultIndexArr[1];
+
+            List<int> resultInts = new List<int>();
+            for (int j = indexStart; j < indexEnd; j++)
+            {
+                Console.WriteLine(inputIntsArr[j]);
+                resultInts.Add(inputIntsArr[j]);
+            }
+
+            // Assert the calculated result is equal to the
+            // expected result retrieved from "CompleteTestCaseOutputFile.txt"
+            Assert.That(resultInts, Is.EqualTo(expectedOutputResultArr));
+        }
     }
 
-    [Test]
-    public void CalculateSolution_ShouldReturnExpectedOutputTest2()
+    private List<string> readLinesFromFile(string filePath)
     {
-        // Assign
-        int[] inputTest = { 6, 2, 4, 3, 1, 5, 9 };
-        int[] expectedResultArr = { 4, 7 };
+        var lines = File.ReadLines(filePath);
+        List<string> linesList = new List<string>();
+        foreach (var line in lines)
+        {
+            linesList.Add(line);
+        }
 
-        // Act
-        int[] resultArr = _solutionAlgorithm.calculateSolution(inputTest);
+        return linesList;
+    }
 
-        // Assert
-        Assert.That(resultArr, Is.EqualTo(expectedResultArr));
+    private int[] readInputIntsArr(string line)
+    {
+        string[] inputStrArr;
+        inputStrArr = line.Split(' ');
+        return Array.ConvertAll(inputStrArr, int.Parse);
     }
 }
